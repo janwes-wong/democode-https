38e70bfa8a44838b3b91afefc1bc5bfafac6a484
janwes <hzwjanwes@outlook.com>
2021-03-12 17:13:10 +0800

    first commit

---
+++ src/main/java/com/janwes/controller/ItemController.java
@@ -0,0 +1,14 @@
+package com.janwes.controller;
+
+/**
+ * @author Janwes
+ * @version 1.0
+ * @package com.janwes.controller
+ * @date 2021/3/12 17:09
+ * @description
+ */
+public class ItemController {
+    public static void main(String[] args) {
+        System.out.println("shopping mall");
+    }
+}
